22aa838455b54a412efc65659971ba0e68081b09
Mohiden abdulkarim <mohidenabdi@hotmail.com>
2021-06-09 12:52:00 +0300

    updated

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -4,41 +4,57 @@ import { useForm } from 'react-hook-form';
 import Axios from 'axios';
 import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
+import { css } from "@emotion/react";
+import { ClipLoader, BarLoader } from "react-spinners";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const HeaderSearch = () => {
+  const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState('#000')
   const navigate = useNavigate();
   const [error, setError] = useState(false);
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  // const [] = useState();
-  const onSubmit = data => {
+
+  const login = async () => {
+
+  }
+  const onSubmit = async data => {
+    setLoading(true);
     const body = {
       name: data.username,
       password: data.password
     }
 
+    const resp = await Axios.post("https://ahlulquran.herokuapp.com/admin/login", body);
+    if (!resp.data.errorMsg) {
+      setError(false);
+      Cookies.set('access-token', resp.data.accessToken, { expires: 7 });
+      window.location.replace("/dashboard")
+      return;
+    }
+    setError(true);
+    setLoading(false);
 
+    // Axios({
+    //   method: "POST",
+    //   url: "https://ahlulquran.herokuapp.com/admin/login",
+    //   data: body,
 
-    Axios({
-      method: "POST",
-      url: "https://ahlulquran.herokuapp.com/admin/login",
-      data: body,
+    // }).then(res => {
+    //   if (!res.data.errorMsg) {
+    //     setError(false);
+    //     Cookies.set('access-token', res.data.accessToken, { expires: 7 });
+    //     window.location.replace("/dashboard")
+    //     return;
+    //   }
+    //   setError(true);
 
-    }).then(res => {
-      if (!res.data.errorMsg) {
-        setError(false);
-        Cookies.set('access-token', res.data.accessToken, { expires: 7 });
-        // navigate("/dashboard");
-        window.location.replace("/dashboard")
-        return;
-      }
-      setError(true);
-      // Swal.fire({
-      //   icon: 'error',
-      //   title: 'Oops...',
-      //   text: 'Please enter valid credential!',
-      // })
-
-    }).catch(err => console.log(err))
+    // }).catch(err => console.log(err))
   }
 
   return (
@@ -59,19 +75,23 @@ const HeaderSearch = () => {
                 <input type='text' placeholder='Username' {...register('username')} />
               </div>
               <div className='col-sm-12'>
-                <input type='text' placeholder='Password' {...register('password')} />
+                <input type='password' placeholder='Password' {...register('password')} />
               </div>
               <div className='col-md-12 col-sm-12 col-lg-12 mt-5'>
                 {/* <Link to="/account/register" className='thm-btn brd-rd40' type='submit'>
                   Login
                 </Link> */}
                 {
-                  error && 
-                <h5 className="text-danger mb-4">Please enter valid info</h5>
+                  error &&
+                  <h5 className="text-danger mb-4">Please enter valid info</h5>
                 }
-                
-                
-                <button type="submit" className="thm-btn brd-rd40">Login</button>
+
+                {
+                  loading ?
+                    <BarLoader color={color} loading={true} css={override} size={150} />
+                    :
+                    <button type="submit" className="thm-btn brd-rd40">Login</button>
+                }
               </div>
             </div>
           </form>
--- src/components/admin/Main.tsx
+++ src/components/admin/Main.tsx
@@ -1,27 +1,82 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
+import Axios from 'axios';
+import { SiAddthis } from 'react-icons/si'
+import { useForm } from 'react-hook-form';
+
+
+
+
 const Main = ({ users }: any) => {
-  // const [users, setUsers] = useState(users);
+  const [allUsers, setAllUsers] = useState(users);
+  const [children, setChildren] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  const addSerialNumber = (id: any) => {
+    console.log(id);
+    Swal.fire({
+      title: 'Add serial number',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Add',
+      showLoaderOnConfirm: true,
+      preConfirm: (serial) => {
+        return Axios({
+          method: "POST",
+          url: "https://ahlulquran.herokuapp.com/admin/add-serial",
+          data: { id: id, serialNumber: serial }
+        })
+          .then(response => {
+            // if (!response.ok) {
+            //   throw new Error(response.statusText)
+            // }
+            // return response.json()
+            console.log(response);
+          })
+          .catch(error => {
+            Swal.showValidationMessage(
+              `Request failed: ${error}`
+            )
+          })
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: `Added Successfully!`,
+        }).then(confirm => {
+          if (confirm.isConfirmed) {
+            window.location.reload(false);
+          }
+        })
+      }
+    })
+  }
+  const showChildrens = (children: any) => {
+    console.log(children);
+    setChildren(children);
+  }
   return (
     <main className='col-md-9 ms-sm-auto col-lg-10 px-md-4'>
       <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
         <h1 className='h2'>Dashboard</h1>
-        <div className='btn-toolbar mb-2 mb-md-0'>
-          <div className='btn-group me-2'>
-            <button type='button' className='btn btn-sm btn-outline-secondary'>
-              Share
-            </button>
-            <button type='button' className='btn btn-sm btn-outline-secondary'>
-              Export
-            </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col">
+              <input type="date" className="form-control" placeholder="From" />
+            </div>
+            <div className="col">
+              <input type="date" className="form-control" placeholder="To" />
+            </div>
+            <button type="submit" className="btn btn-primary">Search</button>
           </div>
-          <button
-            type='button'
-            className='btn btn-sm btn-outline-secondary dropdown-toggle'
-          >
-            <span data-feather='calendar' />
-            This week
-          </button>
-        </div>
+        </form>
+
+
       </div>
       <h2>Registered People</h2>
       <div className='table-responsive'>
@@ -38,10 +93,11 @@ const Main = ({ users }: any) => {
               <th>Teacher</th>
               <th>Course</th>
               <th>Serial Number</th>
+              <th>Children</th>
             </tr>
           </thead>
           <tbody>
-            { users !== undefined ?
+            {users !== undefined ?
               users.map((user: any, idx: any) => (
                 <tr key={idx}>
                   <td>{idx}</td>
@@ -53,12 +109,75 @@ const Main = ({ users }: any) => {
                   <td>{user.registeredDate}</td>
                   <td>{user.teacher}</td>
                   <td>{user.course}</td>
-                  <th>
-                    <button className="btn">Set</button>
-                  </th>
+                  <td>
+                    {
+                      user.serialNumber ?
+                        user.serialNumber
+                        : (
+                          <>
+                            <SiAddthis style={{ color: "green", fontSize: "2rem", cursor: 'pointer' }} onClick={() => addSerialNumber(user._id)} />
+                          </>
+                        )
+                    }
+                  </td>
+                  <td>
+                    {
+                      user.children.length > 0 ?
+                        (
+                          <>
+                            <button className="btn btn-danger" onClick={() => showChildrens(user.children)} data-toggle="modal" data-target="#exampleModal">Show</button>
+                            <div className="modal fade" id="exampleModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                  <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Children</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">×</span>
+                                    </button>
+                                  </div>
+                                  <div className="modal-body">
+                                    <table className="table">
+                                      <thead>
+                                        <tr>
+                                          <th scope="col">#</th>
+                                          <th scope="col">Fname</th>
+                                          <th scope="col">Lname</th>
+                                          <th scope="col">Age</th>
+                                          <th scope="col">Nationality</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        {
+                                          children.map((child: any, idx: any) => (
+                                            <tr key={idx}>
+                                              <td>{idx + 1}</td>
+                                              <td>{child["forename" + idx]}</td>
+                                              <td>{child["surname" + idx]}</td>
+                                              <td>{child["age" + idx]}</td>
+                                              <td>{child["nationalty" + idx]}</td>
+
+                                            </tr>
+                                          ))
+                                        }
+
+                                      </tbody>
+                                    </table>
+
+                                  </div>
+                                  <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </>
+
+                        ) : "no children"
+                    }
+                  </td>
                 </tr>
-                
-                
+
+
               ))
               :
               <h5>No users registered yet !!</h5>
@@ -66,6 +185,12 @@ const Main = ({ users }: any) => {
           </tbody>
         </table>
       </div>
+
+
+
+
+
+
     </main>
   );
 };
